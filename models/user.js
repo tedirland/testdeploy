ae95f9dfce66120b2297e4dbc7de3b1c9155203f
Ted Irland <irlandth@gmail.com>
2021-04-25 15:17:14 -0500

    adding logic to create a new user in the database following the auth workflow

--- models/user.js
+++ models/user.js
@@ -1,2 +1,10 @@
 const mongoose = require("mongoose")
-const { Schema } = mongoose
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  googleId: String,
+  name: String,
+  email: String,
+})
+
+mongoose.model("user", userSchema)
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport")
 const GoogleStrategy = require("passport-google-oauth20").Strategy
+const mongoose = require("mongoose")
 const keys = require("../config/keys")
+
+const User = mongoose.model("user")
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,11 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken)
-      console.log("refresh token", refreshToken)
-      console.log("profile", profile)
+      new User({
+        googleId: profile.id,
+        name: profile.displayName,
+      }).save()
+      console.log(profile)
     }
   )
 )
